44cc1266440fb7e15831f834223cf6d05f00122c
Abdelrahman Eladawy <3adawy@users.noreply.github.com>
2018-03-27 18:46:12 +0200

    Add files via upload

--- apb_test/apb_interconnect.v
+++ apb_test/apb_interconnect.v
@@ -26,6 +26,7 @@ module apb_interconnect
     )
   
   (
+    // MASTER PORT (peripheral port)
  //  input                         PSLVERR,
      input                         PCLK,
      input                         PRESET,
@@ -34,84 +35,155 @@ module apb_interconnect
      output reg [addr_width-1:0]   PADDR,
      
      input                      PREADY,
-     output                     PSEL,
+     output  reg                PSEL,
      output  reg                PENABLE,
-     output                     PWRITE
-      
+     output  reg                PWRITE,
+     
+     
+         // SLAVE PORT (PULPino interface)
+
+     input                                          psel_i,   
+     input                                          penable_i,
+     input                                          pwrite_i,
+     input      [31:0]                              paddr_i,
+     input      [31:0]                              pwdata_i,
+     output reg [31:0]                              prdata_o,
+     output reg                                     pready_o
+ //  output                                         pslverr_o,
+
+     
     );
+    
     reg [addr_width-1:0]  adrs_reg ;
     reg [data_width-1:0]  data_reg ;
-    reg [1:0] state ;
-    
+    reg [1:0] state1 ;
+ //   reg [1:0] state2 ;
+    //   state1 = 0;
+     //  state2 = 0;
     parameter IDLE   = 0,
-              SETUP  = 1,
-              ACCESS = 2;
-    parameter Ready = 0,
-              NotReady = 1;          
-    
-    
-    always @(posedge PCLK)
-       
-       case (state)
-         
-         IDLE : if (PSEL & PENABLE)
-                  state <= ACCESS;
-                else if (PSEL)
-                  state <= SETUP;  
-                else
-                  state <= IDLE;
-         
-         SETUP : if (PWRITE)     
-                   begin
-                    data_reg = PWDATA   ;
-                    PADDR  = adrs_reg ;
-                    PENABLE <= 1;                             // lazem hna a3'yr el enable a5liha b 1
-                    state <= ACCESS;
-                   end
-                  
+               SETUP  = 1,
+               ACCESS = 2;
+     parameter Ready = 1,
+               NotReady = 0;          
+     
+     
+     always @(posedge PCLK)
+       begin  
+        case (state1)
+          
+          IDLE : if (PSEL && PENABLE)
+                   state1 <= ACCESS;
+                 else if (PSEL)
+                   state1 <= SETUP;  
+                 else
+                   state1 <= IDLE;
+          
+          SETUP :
+                 state1 <= ACCESS;
+          /* if (PWRITE)     
+                    begin
+                    // PWDATA = pwdata_i ;
+                    // PADDR  = paddr_i ;
+                    // PENABLE <= 1;                             // lazem hna a3'yr el enable a5liha b 1
+                     state1 <= ACCESS;
+                    end
+                   
                   else if (!PWRITE)
-                   begin
-                    PADDR = adrs_reg  ;
-                    PENABLE <= 1;                           // lazem hna a3'yr el enable a5liha b 1
-                    state <= ACCESS;
-                   end 
-             
-         ACCESS : if (PENABLE & PSEL)
-                   case (PREADY)
-                    Ready : if (PWRITE)
-                            begin 
-                           //   tx_memoey = data_reg;
-                           //   address_in_tx_memory = adrs_reg; 
-                               PENABLE <= 0 ;          // i think en hna el enable lazem tb2a b zero b3d ma a-check 3la el PREADY Lsa b Zero wla La2 
-                              // always @(!PREADY)     // wait state
-                            
-                              state <= ACCESS;
-                             end
-                           else
-                             begin
-                            //   data_reg = rx_fifo;
-                                data_reg = PRDATA ;
-                                PENABLE <= 0 ;           // i think en hna el enable lazem tb2a b zero b3d ma a-check 3la el PREADY Lsa b Zero wla La2
-                                // always @(!PREADY)      // wait state
-           
-                                state <= ACCESS ;
+                    begin
+                    // PADDR = paddr_i ;
+                    // PENABLE <= 1;                           // lazem hna a3'yr el enable a5liha b 1
+                     state1 <= ACCESS;
+                    end 
+              */
+          ACCESS : if (PENABLE && PSEL)
+                    case (PREADY)
+                     Ready : if (PWRITE)
+                             begin 
+                                // el mafrod yb2a fe wait one cycle b3d kda ntsna el Pready tt8yar                       
+                                PENABLE <= 0 ;          // i think en hna el enable lazem tb2a b zero b3d ma a-check 3la el PREADY Lsa b Zero wla La2 
+                                state1 <= ACCESS;
                               end
-                  NotReady :  if (PSEL)
-                               state <= SETUP;
-                              else 
-                               state <= IDLE;         
-                      endcase                 
-                 
-                  else if (PSEL & PREADY)
-                       state <= SETUP ; 
-                       
-                    else                                 //include the state of (PREADY=1 and PENABLE=0 and PSEL=0)
-                        state <= IDLE ;   
+                            else
+                              begin
+                                 // el mafrod yb2a fe wait one cycle b3d kda ntsna el Pready tt8yar
+                                 PENABLE <= 0 ;           // i think en hna el enable lazem tb2a b zero b3d ma a-check 3la el PREADY Lsa b Zero wla La2
+                                 state1 <= ACCESS;
+                               end
+ 
+                   NotReady :  if (PSEL)
+                                state1 <= SETUP;
+                               else 
+                                state1 <= IDLE;         
+                       endcase                 
+                  
+                   else if (PSEL & PREADY)
+                        state1 <= SETUP ; 
                         
-         endcase   
-    
+                   else                                 //include the state of (PREADY=1 and PENABLE=0 and PSEL=0)
+                        state1 <= IDLE ;   
+                         
+          endcase   
+     end
    // if (PENABLE & PSEL & PREADY & PSLVERR)
     //DO SOMETHING ABOUT IT  
     //YET TO BE CONSIDERED (DEPENDS ON THE PERIPHERAL BEHAVIOUR)
     
-endmodule
+         
+       // Interface with PULPino    
+       always @(posedge PCLK)
+       
+          begin  // always block begin
+          
+             if (penable_i && psel_i)    //access state
+                begin       // if begin
+                    PSEL = 1; 
+                    
+                      if (pwrite_i)
+                          begin   // if begin
+                                
+                                PENABLE = 1;                    // pwdata bta3t el peripheral = pwdata_i; 
+                                pready_o = 1;
+                                PWDATA = pwdata_i ;
+                                state1 <= ACCESS;
+                          end   // if end
+                    
+                      else
+                          begin    // else begin
+                                PENABLE = 1; 
+                                pready_o = 1;
+                                prdata_o = PRDATA ;                   //  prdata_o = prdata bta3t el peripheral ; 
+                                state1 <= ACCESS;
+                          end     // else end
+                          
+                  pready_o <= 0;       // set pready to low 
+                  PSEL <= 0;
+                end     // if end
+              
+             else if (psel_i)
+                    begin     // else if begin
+                          PSEL = 1;
+                          PADDR = paddr_i ;                           // el address line for the periperal PADDR = the address from pulpino  paddr_i 
+                          pready_o = 0;
+                            if (!pwrite_i)
+                              begin 
+                               PWRITE = 0 ;
+                               prdata_o = PRDATA ;
+                              end
+                            else 
+                              begin 
+                               PWRITE = 1 ;
+                               PWDATA = pwdata_i ;  
+                              end
+
+                          state1 <= SETUP;
+                    end      // else if end
+             else  
+                    begin    // else begin 
+                          pready_o = 0;
+                          state1 <= IDLE;
+                    end    // else end
+           
+           end   // always block end
+  
+    
+   endmodule
